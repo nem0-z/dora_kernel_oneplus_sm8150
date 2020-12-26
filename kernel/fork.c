423cfc3b710b8ae8d5936b1e68420daf35f7ab69
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2020-12-27 02:05:19 +0530

    kernel/fork: Compile app launch boost only if CIB is enabled
    Signed-off-by: Zlatan Radovanovic <zlatan.radovanovic@fet.ba>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2245,11 +2245,13 @@ long _do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
+#ifdef CONFIG_CPU_INPUT_BOOST
 	/* Boost CPU and DDR bus to the max for 50 ms when userspace launches an app */
   if (task_is_zygote(current)) {
     cpu_input_boost_kick_max(50);
 		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
   }
+#endif /* CONFIG_CPU_INPUT_BOOST */
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
