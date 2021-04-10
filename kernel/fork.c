3519b0c150fbb8e015c55877fadc8e3e099cf380
Zlatan Radovanovic <zlatan.radovanovic@fet.ba>
2021-04-10 07:38:54 +0200

    kernel/fork: Boost CPU for a short amount of time on app launch
    Signed-off-by: Zlatan Radovanovic <zlatan.radovanovic@fet.ba>

--- kernel/fork.c
+++ kernel/fork.c
@@ -96,6 +96,7 @@
 #include <linux/scs.h>
 #include <linux/simple_lmk.h>
 #include <linux/devfreq_boost.h>
+#include <linux/cpu_input_boost.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -2228,9 +2229,11 @@ long _do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
-	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
-	if (task_is_zygote(current))
+	/* Boost CPU and DDR bus to the max for 50 ms when userspace launches an app */
+  if (task_is_zygote(current)) {
+    cpu_input_boost_kick_max(50);
 		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
+  }
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
