c682c7d3400f7c88ff8d1d087635535d19747349
atndko <z1281552865@gmail.com>
2021-04-12 21:49:41 +0800

    fs: fuse: Remove OnePlus changes
    Signed-off-by: atndko <z1281552865@gmail.com>
    Signed-off-by: Zlatan Radovanovic <zlatan.radovanovic@fet.ba>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1399,7 +1399,6 @@ struct task_struct {
 	atomic64_t cpu_dist[8];
 	atomic64_t total_cpu_dist[8];
 
-	struct fuse_package *fpack;
 	/* CPU-specific state of this task: */
 	struct thread_struct		thread;
 
@@ -1409,12 +1408,6 @@ struct task_struct {
 	 *
 	 * Do not put anything below here!
 	 */
-};
-
-struct fuse_package {
-	bool fuse_open_req;
-	struct file *filp;
-	char *iname;
 };
 
 static inline struct pid *task_pid(struct task_struct *task)
