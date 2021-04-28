d6940cfa5a1a4c56e14928bba0b075cd59c13c63
Zlatan Radovanovic <zlatan.radovanovic@fet.ba>
2021-04-28 09:04:45 +0200

    treewide: Silence more logspam
    Signed-off-by: Zlatan Radovanovic <zlatan.radovanovic@fet.ba>

--- drivers/input/fingerprint/goodix/gf_spi.c
+++ drivers/input/fingerprint/goodix/gf_spi.c
@@ -720,7 +720,7 @@ static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 
 	if (val != FB_EARLY_EVENT_BLANK)
 		return 0;
-	pr_info("[info] %s go to the goodix_fb_state_chg_callback value = %d\n",
+	pr_debug("[info] %s go to the goodix_fb_state_chg_callback value = %d\n",
 			__func__, (int)val);
 	gf_dev = container_of(nb, struct gf_dev, notifier);
 
