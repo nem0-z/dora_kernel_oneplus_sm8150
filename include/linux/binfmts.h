d9fa4c1a0ee41a18ce970e02c569a7e4e0b85334
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2021-05-05 22:34:05 +0200

    exec: Update boosted values to match OxygenOS 11

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -155,10 +155,8 @@ static inline bool task_is_booster(struct task_struct *tsk)
 
 	get_task_comm(comm, tsk);
 	return !strcmp(comm, "NodeLooperThrea") ||
-	       !strcmp(comm, "power@1.2-servi") ||
-	       !strcmp(comm, "power@1.3-servi") ||
-	       !strcmp(comm, "perf@1.0-servic") ||
-	       !strcmp(comm, "perf@2.0-servic") ||
+	       !strcmp(comm, "powermanager@1.1-servi") ||
+	       !strcmp(comm, "perf@2.2-servic") ||
 	       !strcmp(comm, "init.qcom.post_");
 }
 
