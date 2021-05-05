1e22a6aac81e42826858a30a414eb31e9f54559b
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2021-05-05 22:21:38 +0200

    exec: Do not boost init. Otherwise, bootanimation is laggy.

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -154,7 +154,7 @@ static inline bool task_is_booster(struct task_struct *tsk)
 	char comm[sizeof(tsk->comm)];
 
 	get_task_comm(comm, tsk);
-	return !strcmp(comm, "init") || !strcmp(comm, "NodeLooperThrea") ||
+	return !strcmp(comm, "NodeLooperThrea") ||
 	       !strcmp(comm, "power@1.2-servi") ||
 	       !strcmp(comm, "power@1.3-servi") ||
 	       !strcmp(comm, "perf@1.0-servic") ||
