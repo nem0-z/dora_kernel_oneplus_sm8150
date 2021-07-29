b4158074f0f4eaaaf3d16e6e3baa1bcb4791fca1
Zlatan Radovanovic <zlatan@bicomsystems.com>
2021-07-29 23:38:21 +0200

    treewide: Cleanup bunch of logging

--- drivers/misc/audio_sensor.c
+++ drivers/misc/audio_sensor.c
@@ -139,7 +139,7 @@ static ssize_t sensor_write(struct file *file,
 	if (sscanf(kbuf, "%d", &state))
 		sensor_data.state = state;
 
-	pr_info("%s: state is %#x\n", __func__, sensor_data.state);
+	pr_debug("%s: state is %#x\n", __func__, sensor_data.state);
 
 	atomic_set(&sensor_data.read_ready, 1);
 	wake_up(&sensor_data.wait);
@@ -180,7 +180,7 @@ unsigned int sensor_poll(struct file *file, struct poll_table_struct *table)
 	if (atomic_read(&sensor_data.read_ready))
 		mask |= POLLIN | POLLRDNORM;
 
-	pr_info("%s: exit mask is %#x\n", __func__, mask);
+	pr_debug("%s: exit mask is %#x\n", __func__, mask);
 	return mask;
 }
 
@@ -195,7 +195,7 @@ static long share_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 	switch (cmd) {
 	case IOCTL_AUDIO_SEND_ACK:
 		data->ack = arg == 1 ? 1 : 0;  //1 success, 0 fail
-		pr_info("%s: send ack %d\n", __func__, data->ack);
+		pr_debug("%s: send ack %d\n", __func__, data->ack);
 		break;
 
 	case IOCTL_SENSOR_RECEIVE_ACK:
@@ -211,7 +211,7 @@ static long share_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 		break;
 
 	default:
-		pr_info("%s: INVAILD cmd\n", __func__);
+		pr_debug("%s: INVAILD cmd\n", __func__);
 		break;
 	}
 
