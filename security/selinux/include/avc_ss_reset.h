feb4bf828dd88143a76ef6b0f125358b6ded066c
jhonboy121 <alfredmathew05@gmail.com>
2021-10-28 20:13:56 +0530

    Revert "init: userland_worker: Initial bring up"
    This reverts commit 3f9f65d43f3d423510a3335779734a49d30f23f0.

--- security/selinux/include/avc_ss_reset.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-/*
- * Access vector cache interface for the security server.
- *
- * Author : Stephen Smalley, <sds@tycho.nsa.gov>
- */
-
-int avc_ss_reset(struct selinux_avc *avc, u32 seqno);
