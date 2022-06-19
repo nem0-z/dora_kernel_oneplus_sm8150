3ecac1765216b1d573e5b156115c7938e151a74b
Zlatan Radovanovic <zlatan.radovanovic@fet.ba>
2022-06-19 09:03:56 +0200

    soc/qcom/smp2p: Remove left parts of wakeup_source
    This amends commit 9c512b9427f1bc7fca92858d6d2de4eba4a3254d ("soc/qcom: Checkout to 5.4 state")
    
    Signed-off-by: Zlatan Radovanovic <zlatan.radovanovic@fet.ba>

--- drivers/soc/qcom/smp2p.c
+++ drivers/soc/qcom/smp2p.c
@@ -650,12 +650,6 @@ static int qcom_smp2p_probe(struct platform_device *pdev)
 		}
 	}
 
-	smp2p->ws = wakeup_source_register(&pdev->dev, "smp2p");
-	if (!smp2p->ws) {
-		ret = -ENOMEM;
-		goto unwind_interfaces;
-	}
-
 	/* Kick the outgoing edge after allocating entries */
 	qcom_smp2p_kick(smp2p);
 
@@ -665,15 +659,12 @@ static int qcom_smp2p_probe(struct platform_device *pdev)
 					"smp2p", (void *)smp2p);
 	if (ret) {
 		dev_err(&pdev->dev, "failed to request interrupt\n");
-		goto unreg_ws;
+		goto unwind_interfaces;
 	}
 
 	enable_irq_wake(smp2p->irq);
 
 	return 0;
-
-unreg_ws:
-	wakeup_source_unregister(smp2p->ws);
 
 unwind_interfaces:
 	list_for_each_entry_safe(entry, next_entry, &smp2p->inbound, node) {
@@ -703,8 +694,6 @@ static int qcom_smp2p_remove(struct platform_device *pdev)
 	struct qcom_smp2p *smp2p = platform_get_drvdata(pdev);
 	struct smp2p_entry *entry;
 	struct smp2p_entry *next_entry;
-
-	wakeup_source_unregister(smp2p->ws);
 
 	list_for_each_entry_safe(entry, next_entry, &smp2p->inbound, node) {
 		irq_domain_remove(entry->domain);
@@ -760,7 +749,6 @@ static int qcom_smp2p_restore(struct device *dev)
 		}
 	}
 
-	smp2p->ws = wakeup_source_register(&pdev->dev, "smp2p");
 	enable_irq_wake(smp2p->irq);
 	/* Kick the outgoing edge after allocating entries */
 	qcom_smp2p_kick(smp2p);
