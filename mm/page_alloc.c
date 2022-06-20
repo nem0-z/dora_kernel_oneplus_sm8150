f5285097935ebf82985400bd455c4b9424138dcc
Zlatan Radovanovic <zlatan.radovanovic@fet.ba>
2022-06-20 08:10:57 +0200

    Revert "mm/page_alloc: boost CPU/RAM a bit earlier in slowpath"
    This reverts commit f20ee41f4e5d7d305c62bc881880a2eeb58e7aaf.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4280,10 +4280,6 @@ retry:
 			(gfp_mask & __GFP_FS))
 		goto nopage;
 
-	/* Boost when memory is low so allocation latency doesn't get too bad */
-	cpu_input_boost_kick_max(250);
-	devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 250);
-
 	/* Try direct reclaim and then allocating */
 	if (!used_vmpressure)
 		used_vmpressure = vmpressure_inc_users(order);
